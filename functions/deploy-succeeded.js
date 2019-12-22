0f85209488ad466d8b5e62b52bfb4a3505a41ee0
Nick Ball <nicholaspball@gmail.com>
2019-12-22 11:49:35 -0800

    Add functions

---
+++ functions/deploy-succeeded.js
@@ -0,0 +1,33 @@
+const fetch = require("node-fetch");
+
+const { DATO_STATUS_URL } = process.env;
+
+const data = JSON.stringify({ status: "success" });
+
+exports.handler = async function(_event, _context) {
+  let response;
+
+  try {
+    response = await fetch(DATO_STATUS_URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: data,
+    });
+  } catch (err) {
+    return {
+      statusCode: err.statusCode || 500,
+      body: JSON.stringify({
+        error: err.message,
+      }),
+    };
+  }
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      data: response,
+    }),
+  };
+};
